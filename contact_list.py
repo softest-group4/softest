8028d48938b1e3adfec39153b84604b14f315961
andrzej <andrzej.b.czajka@gmail.com>
2023-10-13 20:19:34 +0200

    added get_whole_contact_list and implemented check_if_contact_exist_in_contact_list

--- contact_list.py
+++ contact_list.py
@@ -3,13 +3,15 @@ class ContactList:
 
     def __init__(self, contact_list):
         self.contact_list = contact_list
-        pass
 
     def add_new_contact(self, contact):
         pass
 
     def check_if_contact_exists_in_contact_list(self, contact_name):
-        pass
+        contact_names = []
+        for contact in self.contact_list:
+            contact_names.append(contact.name)
+        return contact_name in contact_names
 
     def get_contact_from_contact_list(self, contact_name):
         pass
@@ -25,3 +27,19 @@ class ContactList:
 
     def save_contact_list_to_hard_disc(self):
         pass
+
+    def get_whole_contact_list(self):
+        str_contact_list = f"\nLista kontaktów w Twojej bazie jest następująca:\n"
+        str_contact_list += f"******************************************************\n"
+        for contact in self.contact_list:
+            str_contact_list += f"{contact.name}:\n"
+            if len(contact.address) > 0:
+                str_contact_list += f"adres: {contact.address}\n"
+            if len(contact.phone) > 0:
+                str_contact_list += f"telefon: {contact.phone}\n"
+            if len(contact.mail) > 0:
+                str_contact_list += f"adres mail: {contact.mail}\n"
+            if len(str(contact.birth_date)) > 0:
+                str_contact_list += f"data urodzenia: {contact.birth_date}\n"
+            str_contact_list += f"******************************************************\n"
+        return str_contact_list
--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from contact_list import ContactList
 
 
 def main():
-    contact_1 = Contact("andrzej", "Wrocław", "000000000", "andrzej.b.czajka@gmail.com", date(1970, 1, 1))
-    contact_2 = Contact("marta", "Wrocław", "000000000", "adres@gmail.com", date(1970, 1, 1))
-    contact_3 = Contact("gabriela", "Wrocław", "000000000", "adres@wp.pl", date(1970, 1, 1))
+    contact_1 = Contact("Andrzej", "Wrocław", "000000000", "andrzej.b.czajka@gmail.com", date(1970, 1, 1))
+    contact_2 = Contact("Marta", "Wrocław", "000000000", "adres@gmail.com", date(1970, 1, 1))
+    contact_3 = Contact("Gabriela", "Wrocław", "000000000", "adres@wp.pl", date(1970, 1, 1))
     contacts = ContactList([contact_1, contact_2])
     contacts.add_new_contact(contact_3)
-    print(contacts.get_contact_from_contact_list("marta"))
-    print(contacts.get_contact_from_contact_list("marzena"))
+    print(contacts.get_whole_contact_list())
 
 
 if __name__ == "__main__":
