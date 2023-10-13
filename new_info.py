d7d173d11914a8b0cfde5f632fe5aebd8813539d
Marek511 <133015058+Marek511@users.noreply.github.com>
2023-10-13 18:12:29 +0200

    Editing entries in the class Contact

---
+++ new_info.py
@@ -0,0 +1,15 @@
+    def edit_contact_name(self, new_name):
+        self.name = new_name
+
+    def edit_contact_address(self, new_address):
+        self.address = new_address
+
+    def edit_contact_phone(self, new_phone):
+        self.phone = new_phone
+
+    def edit_contact_email(self, new_email):
+        self.email = new_email
+
+    def edit_contact_birth_date(self, new_birth_date):
+        self.birth_date = new_birth_date
+
