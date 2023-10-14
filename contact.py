5adc54ab92d45a8ffe965eae1fbe27f34f3a3ea4
Marek511 <133015058+Marek511@users.noreply.github.com>
2023-10-14 14:10:52 +0200

    Editing entries in the class Contact Adding an if...else condition in case of invalid info

--- contact.py
+++ contact.py
@@ -13,19 +13,26 @@ class Contact:
         pass
 
     def edit_contact_address(self, new_address):
-        pass
+        self.address = new_address
 
     def edit_contact_name(self, new_name):
-        pass
+        self.name = new_name
 
     def edit_contact_phone(self, new_phone):
-        pass
+        if self.validate_phone_number(new_phone):
+            self.phone = new_phone
+        else:
+            print("Invalid phone number. Correct the phone format")
 
     def edit_contact_email(self, new_email):
-        pass
+        if self.validate_email_address(new_email):
+            self.email = new_email
+        else:
+            print("Invalid email address. Correct the email format")
+
 
     def edit_contact_birth_date(self, new_birth_date):
-        pass
+        self.birth_date = new_birth_date
 
     def get_amount_of_days_to_the_nearest_birthday(self):
         pass
