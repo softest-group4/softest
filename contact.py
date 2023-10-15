838f96dc2ccd404d9ce11cfc661d175890311a67
julotec <135429630+julotec@users.noreply.github.com>
2023-10-15 13:12:30 +0200

    Na początku zaimportowałam funkcję datetime z modułu datetime, od linijki 31. zaimplementowałam kod do funkcji get_whole_contact_lis

--- contact.py
+++ contact.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Contact:
     def __init__(self, name, address, phone, mail, birth_date):
         self.name = name
@@ -5,6 +7,7 @@ class Contact:
         self.phone = phone
         self.mail = mail
         self.birth_date = birth_date
+
 
     def validate_phone_number(self):
         pass
@@ -27,5 +30,36 @@ class Contact:
     def edit_contact_birth_date(self, new_birth_date):
         pass
 
-    def get_amount_of_days_to_the_nearest_birthday(self):
-        pass
+    def get_amount_of_days_to_the_nearest_birthday(contact_list, days):
+        current_date = datetime.now()
+        upcoming_birthday = []
+
+        for contact in contact_list:
+            birthday = datetime.strptime(contact["birth_date"], "%Y -%m -%d")
+            days_until_birthday = birthday.replace(year=current_date.year)
+            time_until_birthday = days_until_birthday - current_date
+
+            if 0 <= time_until_birthday <= days:
+                upcoming_birthday.append(contact)
+
+        return upcoming_birthday
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
