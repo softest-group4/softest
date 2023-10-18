71d0ab20fdeaa6e8e3ff6bdd18a71ffffa01b634
andrzej <andrzej.b.czajka@gmail.com>
2023-10-18 19:24:25 +0200

    no method arguments + return a number

--- pytest_julotec.py
+++ pytest_julotec.py
@@ -4,4 +4,4 @@ from contact import Contact
 
 def test_if_person_with_birthday_at_1st_of_january_indicated_during_nearest_90_days():
     contact = Contact("magda", "", "", "", datetime(1970, 1, 1))
-    assert contact.get_amount_of_days_to_the_nearest_birthday(90) is True
+    assert 60 < contact.get_amount_of_days_to_the_nearest_birthday() < 90
