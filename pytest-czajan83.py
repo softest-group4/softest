7688a9cab6e2bd03e22ec37e81ae3779046f88a6
andrzej <andrzej.b.czajka@gmail.com>
2023-10-22 17:09:24 +0200

    fix only 1st word of argument + fix lt in print help

---
+++ pytest-czajan83.py
@@ -0,0 +1,48 @@
+import pytest
+
+from ui import Ui
+
+
+@pytest.fixture
+def ui():
+    ui = Ui()
+    return ui
+
+
+def test_if_arguments_from_user_input_are_properly_decoded_case_1(ui):
+    ui.inp = "add contact -n Andrzej"
+    assert ui.find_arg_value(f"-n") == f"Andrzej"
+    assert ui.find_arg_value(f"-n1") == f""
+    assert ui.find_arg_value(f"-a") == f""
+    assert ui.find_arg_value(f"-p") == f""
+    assert ui.find_arg_value(f"-m") == f""
+    assert ui.find_arg_value(f"-b") == f""
+    assert ui.find_arg_value(f"-d") == f""
+    assert ui.find_arg_value(f"-t1") == f""
+    assert ui.find_arg_value(f"-c") == f""
+
+
+def test_if_arguments_from_user_input_are_properly_decoded_case_2(ui):
+    ui.inp = "add contact -n Andrzej -a ul. Kiszonki 1, 00-999 Warszawa"
+    assert ui.find_arg_value(f"-n") == f"Andrzej"
+    assert ui.find_arg_value(f"-n1") == f""
+    assert ui.find_arg_value(f"-a") == f"ul. Kiszonki 1, 00-999 Warszawa"
+    assert ui.find_arg_value(f"-p") == f""
+    assert ui.find_arg_value(f"-m") == f""
+    assert ui.find_arg_value(f"-b") == f""
+    assert ui.find_arg_value(f"-d") == f""
+    assert ui.find_arg_value(f"-t1") == f""
+    assert ui.find_arg_value(f"-c") == f""
+
+
+def test_if_arguments_from_user_input_are_properly_decoded_case_3(ui):
+    ui.inp = "add contact -n Andrzej -a ul. Kiszonki 1, 00-999 Warszawa -m adres@adres.pl"
+    assert ui.find_arg_value(f"-n") == f"Andrzej"
+    assert ui.find_arg_value(f"-n1") == f""
+    assert ui.find_arg_value(f"-a") == f"ul. Kiszonki 1, 00-999 Warszawa"
+    assert ui.find_arg_value(f"-p") == f""
+    assert ui.find_arg_value(f"-m") == f"adres@adres.pl"
+    assert ui.find_arg_value(f"-b") == f""
+    assert ui.find_arg_value(f"-d") == f""
+    assert ui.find_arg_value(f"-t1") == f""
+    assert ui.find_arg_value(f"-c") == f""
