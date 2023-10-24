e615809ee577ffdb4b50515326454f1e9f72c15b
Andrzej Czajka <andrzej.b.czajka@gmail.com>
2023-10-24 16:10:19 +0200

    Finalization: setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='softest',
+      version='1',
+      description='contacts organizer, notes organizer',
+      url='https://github.com/softest-group4/softest',
+      author='softest-group4',
+      author_email='softest.group4@gmail.com',
+      license='educational',
+      packages=['pytest', 'Levenshtein', 'pymongo'])
